9a0f8f6875a0709b164f5884b6110d26b2a351ee
Hao Wu <haowu@haowu.me>
2017-06-16 03:41:57 -0500

    update

--- lbjava/src/main/java/edu/illinois/cs/cogcomp/lbjava/learn/vector/OptimizedVector.java
+++ lbjava/src/main/java/edu/illinois/cs/cogcomp/lbjava/learn/vector/OptimizedVector.java
@@ -2,79 +2,61 @@ package edu.illinois.cs.cogcomp.lbjava.learn.vector;
 
 import edu.illinois.cs.cogcomp.core.datastructures.vectors.ExceptionlessInputStream;
 import edu.illinois.cs.cogcomp.core.datastructures.vectors.ExceptionlessOutputStream;
-import gnu.trove.map.hash.TIntDoubleHashMap;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Created by haowu4 on 6/16/17.
  */
-public class SVector implements Vector {
-
-    TIntDoubleHashMap values;
-    int size;
-
-    public SVector() {
-        this(new TIntDoubleHashMap());
-    }
-
-    public SVector(TIntDoubleHashMap values) {
-        this.values = values;
-    }
-
-    protected int getMaxIndex() {
-        return size - 1;
-    }
+public class OptimizedVector implements Vector {
+    Vector realVector;
 
     @Override
     public boolean isDense() {
-        return false;
+        return realVector.isDense();
     }
 
     @Override
     public Vector optimize() {
-        throw new NotImplementedException();
+        return this;
     }
 
     @Override
     public double get(int i) {
-        return values.get(i);
+        return realVector.get(i);
     }
 
     @Override
     public double get(int i, double d) {
-        // TODO(@haowu4): This is not optimized for speed.
-        // Because when the key exists, we need to query the hashmap twice. Will fix this later.
-        return values.containsKey(i) ? values.get(i) : d;
+        return 0;
     }
 
     @Override
     public double set(int i, double v) {
-        return values.put(i, v);
+        return 0;
     }
 
     @Override
     public double set(int i, double v, double d) {
-        throw new NotImplementedException();
+        return 0;
     }
 
     @Override
     public void add(double v) {
-        throw new NotImplementedException();
+
     }
 
     @Override
     public void addAll(DVector v) {
-        throw new NotImplementedException();
+
     }
 
     @Override
     public double remove(int i) {
-        return values.remove(i);
+        return 0;
     }
 
     @Override
     public int size() {
-        return size;
+        return 0;
     }
 
     @Override
